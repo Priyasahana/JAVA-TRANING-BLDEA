214ab9170a4e68eed4182aa516accf46340717fb
supri <supri@LAPTOP-DOAG5RO8>
2023-03-20 03:19:20 +0530

    second commit

---
+++ src/Trainingcodes/test2.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package Trainingcodes;
+
+/**
+ * @author supri
+ *
+ */
+public class test2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
