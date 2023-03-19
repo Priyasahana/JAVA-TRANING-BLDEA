cd4efdb5d4c7116ff7414f7e68915c29b12bae47
supri <supri@LAPTOP-DOAG5RO8>
2023-03-20 02:50:06 +0530

    First commit

---
+++ Traningcodes/Test1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package Traningcodes;
+
+/**
+ * @author supri
+ *
+ */
+public class Test1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
