759502ddc333b3552251838c0f0ecf056e1bade9
supri <supri@LAPTOP-DOAG5RO8>
2023-03-20 09:57:56 +0530

    second commit

--- src/Trainingcodes/test2.java
+++ src/Trainingcodes/test2.java
@@ -8,13 +8,10 @@ package Trainingcodes;
  *
  */
 public class test2 {
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		for(int i=1;i<=5;i++) {
+			System.out.print("*");
+		}
 	}
 
 }
