91c37a39a4edccaf1cfc3158dabf2ac184ccc41e
supri <supri@LAPTOP-DOAG5RO8>
2023-03-20 10:03:39 +0530

    third commit

---
+++ src/Trainingcodes/Test3.java
@@ -0,0 +1,69 @@
+package Trainingcodes;
+import java.util.Scanner;
+public class Test3 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the value of n:");
+		int n=sc.nextInt();
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++)
+			{
+				if (i==0 && j<=n/2 && j!=0 || i==(n/2) && j<n/2 && j!=0 || i==(n-1) && j<(n/2) || j==0 && i<n/2 && i!=0 || j==(n/2) && i>n/2 && i!=(n-1)){				
+					System.out.print("* ");
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			for(int j=0;j<=n;j++) {
+				if(j==0 || i==(n-1) && j<=n/2 || j==(n/2)){
+					System.out.print("* ");
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			for(int j=0;j<=n;j++) {
+				if(j==0 || i==0 && j<n/2 || j==n/2 && i<=n/2 || i==n/2 && j<n/2){
+					System.out.print("* ");
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			for(int j=0;j<=n;j++) {
+				if(j==0 || i==0 && j<n/2 || j==n/2 && i<=n/2 || i==n/2 && j<n/2 || i-j==(n/2)) {
+					System.out.print("* ");
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			for(int j=0;j<=n;j++) {
+				if(j==n/4 || i==0 && j<=n/2 || i==n-1 && j<=n/2) {
+					System.out.print("* ");
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			for(int j=0;j<=n;j++) {
+				if(i==j && i<=n/2 || i+j==n-1 && i<=n/2 || j==n/2 && i>=n/2 && i!=n-1) {
+					System.out.print("* ");
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			for(int j=0;j<=n-1;j++) {
+				if(i==0 || j==0 || j==n-1 || i==n/2){
+					System.out.print("* ");
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
