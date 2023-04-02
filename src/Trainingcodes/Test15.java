9114a7084ff10ad82e371da0244d5e8da58e6579
supri <supri@LAPTOP-DOAG5RO8>
2023-04-02 13:05:20 +0530

    15th commit

---
+++ src/Trainingcodes/Test15.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package Trainingcodes;
+
+/**
+ * @author supri
+ *
+ */
+public class Test15 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
