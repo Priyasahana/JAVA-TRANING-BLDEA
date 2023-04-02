c87e3dbdbe709437991dd6aa235190b1ba7a67c2
supri <supri@LAPTOP-DOAG5RO8>
2023-04-02 13:14:57 +0530

    18th commit

---
+++ src/Trainingcodes/Test18.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package Trainingcodes;
+
+/**
+ * @author supri
+ *
+ */
+public class Test18 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
