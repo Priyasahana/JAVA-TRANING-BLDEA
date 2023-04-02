ffe1afead97e40d8b93f5ebfe23dbe6536b2522b
supri <supri@LAPTOP-DOAG5RO8>
2023-04-02 12:22:55 +0530

    eighth commit

---
+++ src/Trainingcodes/Test6.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package Trainingcodes;
+
+public class program6 {
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++);{
+			System.out.println("# ");
+		}
+	}
+}
+/**
+ * @author supri
+ *
+ */
+public class Test6 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
