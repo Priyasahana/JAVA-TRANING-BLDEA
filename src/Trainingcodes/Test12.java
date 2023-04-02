50e7ad253c203beaeece0914e3e7a7650b195ab1
supri <supri@LAPTOP-DOAG5RO8>
2023-04-02 12:40:25 +0530

    12th commit

---
+++ src/Trainingcodes/Test12.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package Trainingcodes;
+import java.util.Scanner;
+
+public class Test12 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the value if n:");
+		int n=sc.nextInt();
+		int count=n*n;
+		for(int i=1;i<=n;i++);{
+			for(int j=1;j<=n;j++) {
+				System.out.print(count +" ");
+				count--;
+			}
+			System.out.println();
+		}
+
+	}
+
+}
+
